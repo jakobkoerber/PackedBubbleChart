92771b1d308bb38b7e16e593415ada7fe1625648
Jakob Körber <jakob.koerber@tum.de>
2023-02-28 16:17:55 +0100

    Initial Commit

---
+++ PackedBubbleChart/PackedBubbleChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  PackedBubbleChartApp.swift
+//  PackedBubbleChart
+//
+//  Created by Jakob Paul Körber on 28.02.23.
+//
+
+import SwiftUI
+
+@main
+struct PackedBubbleChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
